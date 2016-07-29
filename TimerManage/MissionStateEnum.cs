b90f9a269afb3b43f6e974808b8f8c21f5ca34b7
OldSixLi <1030809514@qq.com>
2016-07-29 16:31:21 +0800

    更改代码

--- TimerManage/MissionStateEnum.cs
+++ TimerManage/MissionStateEnum.cs
@@ -7,25 +7,25 @@ namespace TimerManage
     { /// <summary>
         /// 正局级
         /// </summary>
-        [EnumDescription("show" , "运行中 ")]
+        [EnumDescription("show" , " <i  class='glyphicon glyphicon-ok-sign'></i>运行中 ")]
         ZhengJu = 1 ,
 
         /// <summary>
         /// 副局级
         /// </summary>
-        [EnumDescription("show" , "未运行 ")]
+        [EnumDescription("show" , "<i  class='glyphicon glyphicon-time'></i>未运行 ")]
         Fuju = 2 ,
 
         /// <summary>
         /// 正处级
         /// </summary>
-        [EnumDescription("show" , "暂停 ")]
+        [EnumDescription("show" , "<i  class='glyphicon glyphicon-pause'></i>暂停 ")]
         ZHengChu = 3 ,
 
         /// <summary>
         /// 副处级
         /// </summary>
-        [EnumDescription("show" , "已停止")]
+        [EnumDescription("show" , "  <i  class='glyphicon glyphicon-exclamation-sign'></i>已停止")]
         FuChu = 4 ,
     }
 
