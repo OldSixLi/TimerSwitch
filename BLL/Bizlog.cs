387e872863ac53d985bbe3145f7a10c530044dd1
OldSixLi <1030809514@qq.com>
2016-08-01 14:28:50 +0800

    修改文件

---
+++ BLL/Bizlog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace BLL
+{
+  public  class Bizlog
+    {
+      private readonly DAL.DataAccess dal = new DAL.DataAccess();
+
+      public DataTable GetAllData(Model.TimerMission model, string orderby, int sort, int page, int pageindex, ref int totalpage, ref int index,
+          ref int totalrecord)
+      {
+          return  dal.GetAllData(model, orderby,  sort,  page,  pageindex, ref  totalpage, ref  index,
+          ref  totalrecord);
+      }
+    }
+}
